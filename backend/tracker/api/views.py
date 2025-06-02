d38929b334fcf7e9159d26b3491f52262edc838f
simwawatim <simwawatimothy64@gmail.com>
2025-06-02 12:47:28 +0200

    api again

--- backend/tracker/api/views.py
+++ backend/tracker/api/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from api.serializer.serializers import UserSerializer
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from api.serializer.serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.response import Response
 from main.models import UserProfile
+from django.shortcuts import render
+from rest_framework import status
+
 
 @api_view(['POST'])
 def create_user(request):
@@ -21,13 +21,20 @@ def create_user(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def user_login(request):
-    username = request.data.get('username')
+    email = request.data.get('email')
     password = request.data.get('password')
 
-    user = authenticate(username=username, password=password)
+    if not email or not password:
+        return Response({'error': 'Email and password are required'}, status=400)
+    user = User.objects.filter(email=email).first()
+    if not user:
+        return Response({'error': 'Invalid credentials'}, status=401)
+
+    user = authenticate(username=user.username, password=password)
     if user is not None:
         refresh = RefreshToken.for_user(user)
         try:
--- backend/tracker/tracker/settings.py
+++ backend/tracker/tracker/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'main',
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'tracker.urls'
 
@@ -131,3 +134,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
