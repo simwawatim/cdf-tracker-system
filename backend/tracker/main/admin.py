3d5e62b82db9526553152cd2bc4b71ae4c4149c4
simwawatim <simwawatimothy64@gmail.com>
2025-06-19 20:57:13 +0200

    okay

--- backend/tracker/main/admin.py
+++ backend/tracker/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ProjectCategory, Project, ProjectStatusUpdate, SupportingDocument
+from .models import ProjectCategory, Project, ProjectStatusUpdate, SupportingDocument, UserProfile
 
 admin.site.register(ProjectCategory)
 admin.site.register(Project)
 admin.site.register(ProjectStatusUpdate)
 admin.site.register(SupportingDocument)
+admin.site.register(UserProfile)
