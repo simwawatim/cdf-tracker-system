7ddeb9738b6cf5d662840cfaee039652519d9367
simwawatim <simwawatimothy64@gmail.com>
2025-06-19 22:11:04 +0200

    okay

--- backend/tracker/api/views.py
+++ backend/tracker/api/views.py
@@ -140,7 +140,9 @@ def create_project_status_update(request):
     serializer = ProjectStatusUpdateSerializer(data=request.data)
 
     if serializer.is_valid():
-        status_update = serializer.save(project=project) 
+        status_update = serializer.save(project=project)
+        project.status = status_update.status
+        project.save(update_fields=['status'])
 
         files = request.FILES.getlist('supporting_files')
         for file_obj in files:
